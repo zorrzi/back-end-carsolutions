bd4408dc29be0eedc2b4bf8b483ceca3b2605fe4
zorrzi <eduardovz@al.insper.edu.br>
2024-10-29 18:10:30 -0300

    image urls

--- carros/serializers.py
+++ carros/serializers.py
@@ -1,7 +1,13 @@
+# serializers.py
+
 from rest_framework import serializers
 from .models import Car
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'year', 'brand', 'model', 'mileage', 'purchase_price', 'rental_price', 'image_url', 'is_for_sale', 'is_for_rent']
+        fields = [
+            'id', 'year', 'brand', 'model', 'mileage', 'purchase_price',
+            'rental_price', 'image_url_1', 'image_url_2', 'image_url_3',
+            'is_for_sale', 'is_for_rent', 'is_reserved'
+        ]
